0bd81c692078863d8ebe5a2b1d11aa582fe0537d
Hannah Huang <yunhanh@bu.edu>
2020-03-26 15:25:29 -0400

    add top artist lists and parsing

--- musician_community/authorization_code/app.js
+++ musician_community/authorization_code/app.js
@@ -117,22 +117,32 @@ app.get("/callback", function(req, res) {
           json: true
         };
 
-        console.log(access_token);
+        // console.log(access_token);
 
         // spotifyApi.setAccessToken(access_token);
 
         // use the access token to access the Spotify Web API
         request.get(artists, function(error, response, body) {
-          console.log(body);
+          // console.log(body.items[0].name);
+          var top_list = [];
+          for (i = 0; i < body.items.length; i++) {
+            artist = body.items[i].name;
+            top_list.push(artist);
+          }
+          console.log(top_list);
+          console.log(querystring.stringify(top_list));
+          // var top_list = body.items[0].name;
+
+          // we can also pass the token to the browser to make requests from there
+          res.redirect(
+            "/#" +
+              querystring.stringify({
+                access_token: access_token,
+                refresh_token: refresh_token,
+                top_list: top_list
+              })
+          );
         });
-        // we can also pass the token to the browser to make requests from there
-        res.redirect(
-          "/#" +
-            querystring.stringify({
-              access_token: access_token,
-              refresh_token: refresh_token
-            })
-        );
       } else {
         res.redirect(
           "/#" +
