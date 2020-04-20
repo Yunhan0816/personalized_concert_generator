5daeadd5bbd184ec2194f7dc107c3ba0c23a6d00
Hannah Huang <yunhanh@bu.edu>
2020-04-20 16:05:07 -0400

    update readme

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 
 ## Group 7 Project
 
-### group members: Zhaoguo Zhu, Yinghao Zheng, Yingyun Zhan, Yunhan Huang.
+group members: Yunhan Huang, Zhaoguo Zhu, Yinghao Zheng, Yingyun Zhan.
+
+## Project Summary
+
+- A website for music lover to get personalized up-to-date concert ticket information based on their Spotify listening history and artists preferences.
+
+### Technologies Used:
+
+- Implemented using JavaScript, Express, HTML <br />
+- Utlized Spotify OAuth, Spotify and SeatGeek API, and Firebase Database.
 
 ## Run the Project:
 
-#### cd into musician_community/authorization_code, type
+cd into musician_community/authorization_code, type
 
 ```
 node app.js
--- musician_community/client_credentials/app.js
+++ musician_community/client_credentials/app.js
@@ -10,31 +10,31 @@
 var request = require("request"); // "Request" library
 
 var client_id = "2ad16dcab3504b12a8e1da66110a11e9"; // Your client id
-var client_secret = "95216a023621488f92bca78c3e4052d2"; // Your secret
+var client_secret = ""; // Your secret
 
 // your application requests authorization
 var authOptions = {
   url: "https://accounts.spotify.com/api/token",
   headers: {
     Authorization:
-      "Basic " + new Buffer(client_id + ":" + client_secret).toString("base64")
+      "Basic " + new Buffer(client_id + ":" + client_secret).toString("base64"),
   },
   form: {
-    grant_type: "client_credentials"
+    grant_type: "client_credentials",
   },
-  json: true
+  json: true,
 };
 
-request.post(authOptions, function(error, response, body) {
+request.post(authOptions, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     // use the access token to access the Spotify Web API
     var token = body.access_token;
     var options = {
       url: "https://api.spotify.com/v1/users/jmperezperez",
       headers: {
-        Authorization: "Bearer " + token
+        Authorization: "Bearer " + token,
       },
-      json: true
+      json: true,
     };
 
     // var topArtists = {
@@ -44,7 +44,7 @@ request.post(authOptions, function(error, response, body) {
     //   },
     //   json: true
     // };
-    request.get(options, function(error, response, body) {
+    request.get(options, function (error, response, body) {
       console.log(body);
     });
     // request.get(topArtists, function(error, response, artists) {
